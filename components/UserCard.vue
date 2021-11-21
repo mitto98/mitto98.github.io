bd859eed683fe05591c948a6f69aaf419d088ce1
Mattia Dapino <mattia.dapino@hey.com>
2021-11-21 15:06:30 +0100

    Initial commit

---
+++ components/UserCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex flex-col items-center md:flex-row">
+    <img
+      class="rounded-full w-48 mb-4"
+      src="https://avatars.githubusercontent.com/u/10376295?v=4"
+      alt="Avatar Mattia Dapino"
+    />
+    <div class="flex flex-col justify-center text-3xl md:pl-4">
+      <h1 class="text-4xl font-semibold font-mono mb-1">Mattia Dapino</h1>
+      <div class="flex justify-center md:justify-start gap-x-1">
+        <a
+          v-for="social in socials"
+          :key="social.name"
+          :href="social.url"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="
+            focus:ring-2 focus:ring-current focus:outline-none
+            text-black
+            hover:text-blue-600
+          "
+        >
+          <fa-icon :icon="social.icon" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+export default {
+  computed: {
+    socials: () => [
+      { name: "Github", icon: faGithub, url: "http://github.com/mitto98" },
+      {
+        name: "Linkedin",
+        icon: faLinkedin,
+        url: "https://www.linkedin.com/in/mattiadapino/",
+      },
+    ],
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  box-shadow: -0.5rem 0.5rem black, 0.5rem -0.5rem #cecece;
+  transition: box-shadow 1s ease;
+  &:hover {
+    box-shadow: none;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-16 md:ml-32 md:mt-24">
+    <UserCard />
+    <div class="mt-6 md:mt-4 px-4 md:px-0 text-justify max-w-3xl text-lg">
+      <strong>Ciao!</strong> Mi chiamo Mattia e sono un consulente informatico,
+      specializzato nello sviluppo di interfacce utenti per applicativi web.
+      <br />
+      Sono appassionato di DevOps e docker e credo fortemente
+      nell'industrializzazione del software. Buona parte dei miei progetti sono
+      open source e sono disponibili sul mio
+      <a
+        href="https://github.com/mitto98"
+        target="_blank"
+        rel="noopener noreferrer"
+        >profilo GitHub </a
+      >. <br /><br />
+      Nel tempo libero mi dedico alla fotografia e mi diverto a guardare anime,
+      leggere e a costruire oggetti inutilmente complicati che risolvono
+      problemi inesistenti.
+    </div>
+  </div>
+</template>
+
+<script>
+import "../assets/css/tailwind.scss";
+export default {};
+</script>
